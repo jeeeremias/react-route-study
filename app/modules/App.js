302391c066e84450111fe4786142bc80fc3fcc31
Jeremias Pereira <jeremias.livre@gmail.com>
2017-03-05 13:52:12 -0300

    Using activeClassName Link property

--- app/modules/App.js
+++ app/modules/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router'
+import NavLink from './NavLink'
 
 export default ({children}) => (
     <div>
         <h1>People that make me good</h1>
         <ul role="nav">
-            <li><Link to="/family">Family</Link></li>
-            <li><Link to="/friends">Friends</Link></li>
+            <li><NavLink to="/family">Family</NavLink></li>
+            <li><NavLink to="/friends">Friends</NavLink></li>
         </ul>
         {children}
     </div>
