c327682f0042b37a8ecff55926d97e89010dbc94
Jeremias Pereira <jeremias.livre@gmail.com>
2017-03-05 13:14:13 -0300

    Added new pages and routed them

--- app/index.js
+++ app/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
+import Family from './modules/Family'
+import Friends from './modules/Friends'
 
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={App}/>
+        <Route path="/family" component={Family}/>
+        <Route path="/friends" component={Friends}/>
     </Router>
 ), document.getElementById('app'))
