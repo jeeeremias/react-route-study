d30ced4a9576eb53db74f6682e86b0e0b1949844
Jeremias Pereira <jeremias.livre@gmail.com>
2017-03-05 13:26:04 -0300

    Created a navigation menu using Link component

--- app/modules/App.js
+++ app/modules/App.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 export default () => (
-    <div>Hello, React Router!</div>
+    <div>
+        <h1>Let's know all people that I love</h1>
+        <ul role="nav">
+            <li><Link to="/family">Family</Link></li>
+            <li><Link to="/friends">Friends</Link></li>
+        </ul>
+    </div>
 )
