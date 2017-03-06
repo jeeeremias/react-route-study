1724bc0abb342bb2900c36b01bf6449a08c1c1dc
Jeremias Pereira <jeremias.livre@gmail.com>
2017-03-05 22:56:59 -0300

    Added submit and context router

--- app/modules/Friends.js
+++ app/modules/Friends.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import NavLink from './NavLink'
 
-export default ({children}) => (
-    <div>
-    <h2>Let's know my second family</h2>
-        <ul>
-            <li><NavLink to="/friends/Jennifer">Jennifer</NavLink></li>
-            <li><NavLink to="/friends/Lucas">Lucas</NavLink></li>
-            <li><NavLink to="/friends/Carlos">Carlos</NavLink></li>
-            <li><NavLink to="/friends/Luiza">Luiza</NavLink></li>
-        </ul>
-        {children}
-    </div>
-)
+const Friends = ({children}, context) => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const name = event.target.elements[0].value;
+        const path = `/friends/${name}`;
+        context.router.push(path);
+    };
+    
+    return (
+        <div>
+            <h2>Let's know my second family</h2>
+            <ul>
+                <li><NavLink to="/friends/Jennifer">Jennifer</NavLink></li>
+                <li><NavLink to="/friends/Lucas">Lucas</NavLink></li>
+                <li><NavLink to="/friends/Carlos">Carlos</NavLink></li>
+                <li><NavLink to="/friends/Luiza">Luiza</NavLink></li>
+                <li>Let's became friends!
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="your name"/>{' '}
+                        <button type="submit">Add friend</button>
+                    </form>
+                </li>
+            </ul>
+            {children}
+        </div>
+    )
+}
+
+Friends.contextTypes = {
+    router: React.PropTypes.object
+};
+
+export default Friends;
